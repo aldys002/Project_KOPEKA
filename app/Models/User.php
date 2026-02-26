b5bf245006f048794e55048dbbd2523b519c9252
Asti22 <uun3461@gmail.com>
2026-02-26 21:43:10 +0700

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,41 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // 1. Tambahkan ini agar Laravel tahu Primary Key-nya NIPP (bukan ID)
+    protected $primaryKey = 'nipp';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    // 2. Update fillable agar bisa menyimpan NIPP dan NIK dari Excel
     protected $fillable = [
+        'nipp',
         'name',
-        'email',
+        'nik',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    // 3. Tambahkan Relasi ke tabel Simpanan
+    public function simpanan()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Simpanan::class, 'nipp', 'nipp');
+    }
+
+    // 4. Tambahkan Relasi ke tabel Hutang
+    public function hutang()
+    {
+        return $this->hasOne(Hutang::class, 'nipp', 'nipp');
     }
 }

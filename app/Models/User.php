8ff7fe3890995f9f613cc5e2872f308bd4fe6c47
aldys002 <igidiaaldys@gmail.com>
2026-02-27 02:55:19 +0700

    ganti users jadi anggota

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $table = 'users'; 
+    protected $table = 'anggota'; 
     protected $primaryKey = 'nipp';
     public $incrementing = false;
     protected $keyType = 'string';
